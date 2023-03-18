4d46af463254e6839d2a58f9d9699d3863808a2a
Micah Workman <micahw11@gmail.com>
2023-03-18 19:47:01 -0400

    gdx-template

---
+++ desktop/src/com/micatss/plantsim/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.micatss.plantsim;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.micatss.plantsim.PlantSim;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("plant-sim");
+		new Lwjgl3Application(new PlantSim(), config);
+	}
+}
