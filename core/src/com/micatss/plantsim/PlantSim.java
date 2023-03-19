6e9fba40fa6298bb5c31c940c84bfba455e9c019
Micah Workman <micahw11@gmail.com>
2023-03-18 23:15:58 -0400

    game and screens

--- core/src/com/micatss/plantsim/PlantSim.java
+++ core/src/com/micatss/plantsim/PlantSim.java
@@ -1,31 +1,24 @@
 package com.micatss.plantsim;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class PlantSim extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+public class PlantSim extends Game {
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		this.setScreen(new MainMenuScreen(this));
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		super.dispose();
 	}
 }
